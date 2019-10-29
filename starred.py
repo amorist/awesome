790b6cf40af7772c626fd53ccb10b16ef3f065bd
amorist <amor90s.zhang@gmail.com>
2019-10-29 23:00:35 +0800

    Fixed

--- starred.py
+++ starred.py
@@ -2,5 +2,5 @@
 import os
 starred_output = os.popen('starred --username amorist --sort > README.md', 'r')
 print(starred_output.read())
-git_output = os.popen('git add README.md && git commit -m "update" && git push -u origin master', 'r')
+git_output = os.popen('git add README.md && git commit -m "update" && git push -u origin master -f', 'r')
 print(git_output.read())
